3f6b1039e832d1376544f216cac936a530c6ea50
Mona Dasig <monalizadasig@gmail.com>
2025-02-14 07:47:23 -0800

    refactored code into MVC pattern

---
+++ src/js/model.js
@@ -0,0 +1,36 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(res, data);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(state.recipe);
+  } catch (err) {
+    // Temp error handling
+    console.error(`${err} 💥💥💥💥`);
+    throw err;
+  }
+};
