3b2d882dce2983531bddfedc6660e0e112a758d0
Mona Dasig <monalizadasig@gmail.com>
2025-02-21 10:27:09 -0800

    refactor:  create parentView for  both resultsView and bookmarksView

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,12 +3,15 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
-  render(data) {
+  render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
 
     this._data = data;
     const markUp = this._generateMarkup();
+
+    if (!render) return markUp;
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markUp);
   }
