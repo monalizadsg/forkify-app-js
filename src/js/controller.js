b4b488a72be091eab1a4d96f09f17067f32dcbc1
Mona Dasig <monalizadasig@gmail.com>
2025-02-24 09:56:17 -0800

    clean up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,6 +130,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('BUG');
 };
 init();
