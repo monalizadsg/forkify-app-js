2df3ff24caeed892203ed9fafdf42a20c2d60cbe
Mona Dasig <monalizadasig@gmail.com>
2025-02-24 09:31:49 -0800

    test git commands

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,5 +130,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('BUG');
 };
 init();
